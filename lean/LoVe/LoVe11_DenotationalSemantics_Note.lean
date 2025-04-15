52b2a13337fdd5bdb3fe15bc59d95d438e627e37
rikitoro <>
2025-04-15 21:48:52 +0900

    LoVe11 Note update

--- lean/LoVe/LoVe11_DenotationalSemantics_Note.lean
+++ lean/LoVe/LoVe11_DenotationalSemantics_Note.lean
@@ -107,6 +107,144 @@ theorem le_lfp {α : Type} [CompleteLattice α]
   simp
   exact h
 
+theorem lfp_eq {α : Type} [CompleteLattice α] (f : α → α)
+  (hf : Monotone f) :
+  lfp f = f (lfp f) := by
+  have h : f (lfp f) ≤ lfp f := by
+    apply le_lfp
+    intro a ha
+    apply le_trans
+    . apply hf
+      apply lfp_le
+      apply ha
+    . apply ha
+  apply le_antisymm
+  . apply lfp_le
+    apply hf
+    apply h
+  . apply h
+
+-- ## A Relational Denotational Semantics, Continued
+
+-- def denote : Stmt → Set (State × State)
+--   | Stmt.skip             => Id
+--   | Stmt.assign x a       =>
+--     {st | Prod.snd st = (Prod.fst st)[x ↦ a (Prod.fst st)]}
+--   | Stmt.seq S T          => denote S ◯ denote T
+--   | Stmt.ifThenElse B S T =>
+--     (denote S ⇃ B) ∪ (denote T ⇃ (fun s ↦ ¬ B s))
+--   | Stmt.whileDo B S      =>
+--     lfp (fun X ↦ ((denote S ◯ X) ⇃ B)
+--       ∪ (Id ⇃ (fun s ↦ ¬ B s)))
+
+def denote : Stmt → Set (State × State)
+  | .skip             => Id
+  | .assign x a       => {(s, t) | t = s [x ↦ a s]}
+  | .seq S T          => denote S ◯ denote T
+    -- r₁ ◯ r₂ = {(a, c) | ∃ b, (a, b) ∈ r₁ ∧ (b, c) ∈ r₂}
+  | .ifThenElse B S T =>  (denote S ⇃ B) ∪ (denote T ⇃ (fun s ↦ ¬ B s))
+    -- r ⇃ P = {(a, b) | (a, b) ∈ r ∧ P a}
+  | .whileDo B S      =>
+    lfp (fun X ↦ ((denote S ◯ X) ⇃ B) ∪ (Id ⇃ (fun s ↦ ¬ B s)))
+
+notation (priority := high) "⟦" S "⟧" => denote S
+
+
+
+/- We will prove the following two theorems in the exercise. -/
+
+namespace SorryTheorems
+
+theorem Monotone_comp {α β : Type} [PartialOrder α]
+      (f g : α → Set (β × β)) (hf : Monotone f)
+      (hg : Monotone g) :
+    Monotone (fun a ↦ f a ◯ g a) :=
+  sorry
+
+theorem Monotone_restrict {α β : Type} [PartialOrder α]
+      (f : α → Set (β × β)) (P : β → Prop) (hf : Monotone f) :
+    Monotone (fun a ↦ f a ⇃ P) :=
+  sorry
+
+end SorryTheorems
+
+theorem Monotone_while_lfp_arg (S B) :
+  Monotone (fun X ↦ ⟦S⟧ ◯ X ⇃ B ∪ Id ⇃ (fun s ↦ ¬ B s)) := by
+  apply Monotone_union
+  . apply SorryTheorems.Monotone_restrict
+    apply SorryTheorems.Monotone_comp
+    . apply Monotone_const
+    . apply Monotone_id
+  . apply SorryTheorems.Monotone_restrict
+    apply Monotone_const
+
+-- ## Application to Program Equivalence
+
+def DenoteEquiv (S₁ S₂ : Stmt) : Prop :=
+  ⟦S₁⟧ = ⟦S₂⟧
+
+infix:50 (priority := high) " ~ " => DenoteEquiv
+
+theorem DenoteEquiv.seq_congr {S₁ S₂ T₁ T₂ : Stmt}
+  (hS : S₁ ~ S₂) (hT : T₁ ~ T₂) : S₁; T₁ ~ S₂; T₂ := by
+  simp [DenoteEquiv, denote] at *
+  simp [hS, hT]
+
+theorem DenoteEquiv.if_congr {B} {S₁ S₂ T₁ T₂ : Stmt}
+  (hS : S₁ ~ S₂) (hT : T₁ ~ T₂) :
+  .ifThenElse B S₁ T₁ ~ .ifThenElse B S₂ T₂ := by
+  simp [DenoteEquiv, denote] at *
+  simp [hS, hT]
+
+theorem DenoteEquiv.while_congr {B} {S₁ S₂ : Stmt}
+  (hS : S₁ ~ S₂) :
+  .whileDo B S₁ ~ .whileDo B S₂ := by
+  simp [DenoteEquiv, denote] at *
+  simp [hS]
+
+theorem DenoteEquiv.skip_assign_id {x} :
+  .assign x (fun s ↦ s x) ~ .skip := by
+  simp [DenoteEquiv, denote, Id]
+
+theorem DenoteEquiv.seq_skip_left {S} :
+  .skip; S ~ S := by
+  simp [DenoteEquiv, denote, comp]
+
+theorem DenoteEquiv.seq_skip_right {S} :
+  S; .skip ~ S := by
+  simp [DenoteEquiv, denote, comp]
+
+
+theorem DenoteEquiv.if_seq_while {B S} :
+    Stmt.ifThenElse B (S; Stmt.whileDo B S) Stmt.skip
+    ~ Stmt.whileDo B S :=
+      by
+    simp [DenoteEquiv, denote]
+    apply Eq.symm
+    apply lfp_eq
+    apply Monotone_while_lfp_arg
+
+
+-- ## A Simpler Approach Based on an Inductive Predicate
+
+inductive Awhile (B : State → Prop) (r : Set (State × State)) :
+  State → State → Prop
+  | true {s t u} (hcond : B s)
+    (hbody : (s, t) ∈ r) (hrest : Awhile B r t u) :
+    Awhile B r s u
+  | false {s} (hcond : ¬ B s) :
+    Awhile B r s s
+
+
+def denoteAwhile : Stmt → Set (State × State)
+  | .skip             => Id
+  | .assign x a       =>
+    {(s, t) | t = s [x ↦ a s]}
+  | .seq S T          => denoteAwhile S ◯ denoteAwhile T
+  | .ifThenElse B S T =>
+    (denoteAwhile S ⇃ B) ∪ (denoteAwhile T ⇃ (fun s ↦ ¬ B s))
+  | .whileDo B S      =>
+    {(s, t) | Awhile B (denoteAwhile S) s t}
 
 
 end LoVe
