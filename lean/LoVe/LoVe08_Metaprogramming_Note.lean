196f7ed60ccf33c723d32d6833cb285cf357973c
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2025-07-17 12:29:55 +0900

    update LoVe08 Note

--- lean/LoVe/LoVe08_Metaprogramming_Note.lean
+++ lean/LoVe/LoVe08_Metaprogramming_Note.lean
@@ -14,18 +14,81 @@ namespace LoVe
 /- ## 8.1 Tactic Combinators -/
 
 
-
-theorem repeat'_example :
+theorem repeat_example :
   Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
-  repeat' apply And.intro
+  repeat' apply And.intro    --  なぜか repeat' hoge とした段階で Lean がハングしてしまう
   repeat' apply Even.add_two
   repeat' sorry
 
-theorem repeat'_first_example :
+theorem repeat_first_example :
   Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
   repeat' apply And.intro
   repeat'
     first
     | apply Even.add_two
     | apply Even.zero
+  . show Even 1
+    sorry
+  . show Even 1
+    sorry
+
+theorem all_goals_example :
+  Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
+  repeat' apply And.intro
+  -- all_goals apply Even.add_two -- fails
   repeat' sorry
+
+theorem all_goals_try_example :
+  Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
+  repeat' apply And.intro
+  all_goals try apply Even.add_two -- always succeeds
+  . show Even 2
+    sorry
+  . show Even 5
+    sorry
+  . show Even 1
+    sorry
+  . show Even 0
+    sorry
+
+theorem any_goals_example :
+  Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
+  repeat' apply And.intro
+  any_goals apply Even.add_two -- can fail
+  . show Even 2
+    sorry
+  . show Even 5
+    sorry
+  . show Even 1
+    sorry
+  . show Even 0
+    sorry
+
+
+
+
+/- ## 8.2 Macros -/
+
+macro "intro_and_even" : tactic =>
+  `(tactic|
+    (
+      repeat' apply And.intro
+      any_goals
+        solve
+        | repeat'
+          first
+          | apply Even.add_two
+          | apply Even.zero
+    )
+  )
+
+theorem intro_and_even_example :
+  Even 4 ∧ Even 7 ∧ Even 3 ∧ Even 0 := by
+  intro_and_even
+  . show Even 7
+    sorry
+  . show Even 3
+    sorry
+
+
+/- ## 8.3 The Metaprogramming Monads -/
